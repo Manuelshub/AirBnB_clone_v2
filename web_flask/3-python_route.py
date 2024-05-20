1b8d23f1bf2080589056542b89518afc9e5450d2
Emmanuel <kcemmy03@gmail.com>
2024-05-20 05:26:10 +0100

    Added another route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This Module contains a Flask Application that listens on 0.0.0.0 port 5000
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# Route definition
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    repl = text.replace('_', ' ')
+    return f"C {repl}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    repl = text.replace('_', ' ')
+    return f"Python {repl}"
+
+
+if __name__ == "__main__":
+    # Run the Flask application.
+    app.run(host='0.0.0.0', port=5000)
